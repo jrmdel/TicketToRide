858ba2d7e22b92c8d83ffd39099f1a757c5a29f8
jrmdel <jeremie.deletraz@gmail.com>
2020-11-19 16:33:16 +0100

    Routes and tours are functional

--- src/components/CurrentGame.vue
+++ src/components/CurrentGame.vue
@@ -3,7 +3,7 @@
         <v-row>
             <v-col cols="12">
                 <v-card>
-                    <v-toolbar flat color="quaternary" dark>
+                    <v-toolbar flat color="primary" dark>
                         <v-toolbar-title>Your tickets</v-toolbar-title>
                         <v-spacer></v-spacer>
                         <v-icon large>mdi-ticket-confirmation-outline</v-icon>
@@ -13,15 +13,15 @@
                             <v-row justify="center" justify-sm="space-around">
                                 <v-col cols="auto">
                                     <span class="text-caption">nb of tickets</span>
-                                    <span class="ml-4 text-h3">{{tickets.length}}</span>
+                                    <span class="ml-4 text-h3">{{routes.length+tours.length}}</span>
                                 </v-col>
                                 <v-col cols="auto">
                                     <span class="text-caption">nb completed</span>
-                                    <span class="ml-4 text-h3">{{tickets.length}}</span>
+                                    <span class="ml-4 text-h3">{{computedCompletion}}</span>
                                 </v-col>
                                 <v-col cols="auto">
                                     <span class="text-caption">current score</span>
-                                    <span class="ml-4 text-h3">{{tickets.length}}</span>
+                                    <span class="ml-4 text-h3">{{computedTicketScore}}</span>
                                 </v-col>
                             </v-row>
                         </v-container>
@@ -30,12 +30,15 @@
                         <v-container fluid>
                             <v-row>
                                 <v-col>
-                                    <v-btn @click="openAddTicket">ADD A TICKET</v-btn>
+                                    <v-btn dark x-large color="primary" @click="openAddTicket">
+                                        <v-icon>mdi-card-plus-outline</v-icon>
+                                        <span class="ml-3 mr-1">ADD A TICKET</span>
+                                    </v-btn>
                                 </v-col>
                             </v-row>
                             <v-row>
                                 <v-col cols="12">
-                                    <v-card outlined flat color="accent">
+                                    <v-card outlined flat color="secondary">
                                         <v-card-title class="text-h4 font-weight-light">
                                             Routes
                                             <v-spacer></v-spacer>
@@ -82,6 +85,9 @@
                                             <template v-slot:[`item.status`]="{ item }">
                                                 <v-chip :color="getStatusColor(item.status)" dark>{{ item.status }}</v-chip>
                                             </template>
+                                            <template v-slot:[`item.to`]="{ item }">
+                                                {{item.cities.slice(-1)[0]}}
+                                            </template>
                                         </v-data-table>
                                     </v-card>
                                 </v-col>
@@ -98,39 +104,92 @@
                         <v-spacer></v-spacer>
                         <v-icon large>mdi-train</v-icon>
                     </v-toolbar>
+                    <v-card-subtitle>
+                        <v-container fluid>
+                            <v-row justify="center" justify-sm="space-around">
+                                <v-col cols="auto">
+                                    <span class="text-caption">nb of trains</span>
+                                    <span class="ml-4 text-h3">{{routes.length+tours.length}}</span>
+                                </v-col>
+                                <v-col cols="auto">
+                                    <span class="text-caption">nb of boats</span>
+                                    <span class="ml-4 text-h3">{{computedCompletion}}</span>
+                                </v-col>
+                                <v-col cols="auto">
+                                    <span class="text-caption">current score</span>
+                                    <span class="ml-4 text-h3">{{computedTicketScore}}</span>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-card-subtitle>
                     <v-card-text>
-                        <v-btn>ADD</v-btn>
+                        <v-container fluid>
+                            <v-row justify="center" justify-sm="space-around">
+                                <v-col cols="12">
+                                    <span class="text-h6 tertiary--text">Trains</span>
+                                </v-col>
+                                <v-col>
+                                    <span>
+                                        <v-icon>mdi-train</v-icon>
+
+                                    </span>
+                                </v-col>
+                                <v-col>
+                                    <span>
+                                        <v-icon>mdi-train</v-icon>
+                                        <v-icon>mdi-train</v-icon>
+                                    </span>
+                                </v-col>
+                            </v-row>
+                            <v-row justify="center" justify-sm="space-around">
+                                <v-col cols="12">
+                                    <span class="text-h6 tertiary--text">Boats</span>
+                                </v-col>
+                            </v-row>
+                        </v-container>
                     </v-card-text>
                 </v-card>
             </v-col>
         </v-row>
 
-        <v-dialog v-model="dialogTicket" max-width="600" @click:outside="closeAddTicket">
+        <v-dialog v-model="dialogTicket" max-width="660" @click:outside="closeAddTicket">
             <v-card>
                 <v-toolbar flat color="quaternary" dark>
                     <v-toolbar-title>New Ticket</v-toolbar-title>
                 </v-toolbar>
                 <v-card-text>
-                    <v-form v-model="newTicketForm" ref="newTicketForm">
-                        <v-container fluid>
-                            <v-row align="center">
-                                <v-col cols="12">
-                                    <span class="text-h6 tertiary--text">Cities</span>
-                                </v-col>
-                                <v-col cols="12" md="6">
-                                    <v-select v-model="fromTicket" :rules="simpleRule" color="secondary" hide-details outlined label="From" :items="computedFromCities"></v-select>
-                                </v-col>
-                                <v-col cols="12" md="6">
-                                    <v-select v-model="toTicket" :disabled="!fromTicket" :rules="simpleRule" color="secondary" hide-details outlined label="To" :items="computedToCities"></v-select>
-                                </v-col>
-                            </v-row>
-                        </v-container>
-                    </v-form>
+                    <v-container fluid>
+                        <v-row align="center">
+                            <v-col cols="12">
+                                <span class="text-h6 tertiary--text">Cities</span>
+                            </v-col>
+                            <v-col cols="12" md="6">
+                                <v-autocomplete v-model="fromTicket" :rules="simpleRule" placeholder="Type here" clearable color="secondary" hide-details outlined label="From" :items="computedFromCities"></v-autocomplete>
+                            </v-col>
+                            <v-col cols="12" md="6">
+                                <v-autocomplete v-model="toTicket" :disabled="!fromTicket" placeholder="Type here" :rules="simpleRule" clearable color="secondary" hide-details outlined label="To" :items="toCities"></v-autocomplete>
+                            </v-col>
+                            <v-col cols="12" v-if="foundTickets.length==1">
+                                <v-card flat outlined color="accent">1 match found</v-card>
+                            </v-col>
+                            <v-col cols="12" v-if="foundTickets.length>1">
+                                <span>Select your ticket</span>
+                                <v-card flat outlined>
+                                    <v-data-table v-model="selectedTicket" class="py-6" :headers="headersAddTicket" :items="foundTickets" item-key='id'
+                                    hide-default-footer single-select show-select>
+                                        <template v-slot:[`item.to`]="{ item }">
+                                            {{item.cities.slice(-1)[0]}}
+                                        </template>
+                                    </v-data-table>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </v-container>
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn large text color="accent" @click="closeCreate">CLOSE</v-btn>
-                    <v-btn large :disabled="!newTicketForm" text color="secondary" @click="addTicket">ADD TICKET</v-btn>
+                    <v-btn large text color="accent" @click="closeAddTicket">CLOSE</v-btn>
+                    <v-btn large :disabled="selectedTicket.length==0" text color="secondary" @click="addTicket">ADD TICKET</v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -145,6 +204,17 @@ export default {
         dialogTicket: false,
         newTicketForm: false,
         simpleRule: [v => !!v || "A city is required"],
+        fromTicket:null,
+        toCities: [],
+        toTicket:null,
+        foundTickets: [],
+        selectedTicket: [],
+        headersAddTicket:[
+            {text:"From", align:"start", value:"cities[0]", sortable: true},
+            {text:"To", align:"start", value:"to", sortable: true},
+            {text:"Steps", align:"start", value:"cities.length", sortable: true},
+            {text:"Points", align:"start", value:"points", sortable: true},
+        ],
         tickets: [],
         headersRoutes:[
             {text:"From", align:"start", value:"cities[0]", sortable: true},
@@ -162,7 +232,7 @@ export default {
         ],
         headersTours:[
             {text:"From", align:"start", value:"cities[0]", sortable: true},
-            {text:"To", align:"start", value:"cities[1]", sortable: true},
+            {text:"To", align:"start", value:"to", sortable: true},
             {text:"Steps", align:"start", value:"cities.length", sortable: true},
             {text:"Points", align:"start", value:"points", sortable: true},
             {text:"Status", align:"start", value:"status", sortable: true},
@@ -172,13 +242,72 @@ export default {
         searchTours: "",
         // tours: [],
         tours: [
-            {id: 6, cities:["Anchorage","Vancouver","Winnipeg","Cambridge Bay"], points: 15, status: "Unordered"}
+            {id: 6, cities:["Anchorage","Vancouver","Winnipeg","Cambridge Bay"], points:15, points_failed:-19, points_unorderded:9, status: "Unordered"}
         ],
+        selectVersion: "Around The World"
     }),
+    computed:{
+        computedFromCities:{
+            get(){
+                return [...new Set(this.tickets.map(ticket => ticket.cities).flat())].sort()
+            }
+        },
+        computedCompletion:{
+            get(){
+                return this.routes.filter(route => route.status == "Done").length+this.tours.filter(tour => tour.status != "Fail").length
+            }
+        },
+        computedTicketScore:{
+            get(){
+                let r = (this.routes.length>0) ? this.routes.map(x => (x.status=="Done") ? x.points : -1*x.points).reduce((a, b) => a + b, 0) : 0
+                let t = (this.tours.length>0) ? this.tours.map(x => (x.status=="Fail") ? x.points_failed : (x.status=="Done") ? x.points : x.points_unorderded) : 0
+                return parseInt(r)+parseInt(t)
+            }
+        }
+    },
+    watch:{
+        fromTicket:{
+            handler(value){
+                this.toCities = [];
+                this.toTicket = null;
+                if(value){
+                    let a = this.tickets.filter(ticket => ticket.cities.includes(value)).map(ticket => ticket.cities).flat().sort()
+                    this.toCities = [...new Set(a)].filter(city => city != value)
+                }
+            }
+        },
+        toTicket:{
+            handler(val){
+                this.foundTickets = []
+                if(val != null){
+                    this.foundTickets = this.tickets.filter(ticket => ticket.cities.includes(val)).filter(ticket=>ticket.cities.includes(this.fromTicket))
+                }
+            }
+        },
+        foundTickets:{
+            handler(value){
+                this.selectedTicket = []
+                if(value){
+                    if(value.length == 1) this.selectedTicket = value;
+                }
+            }
+        },
+        selectVersion:{
+            handler(value){
+                this.tickets = [];
+                this.routes = [];
+                this.tours = [];
+                if(value){
+                    this.tickets = (value=="Around The World") ? Tickets.World : Tickets.GreatLakes
+                }
+            }
+        }
+    },
     methods:{
         deleteItem(item){
             let id = this.routes.indexOf(item);
-            this.routes.splice(id,1);
+            if(id < 0) this.tours.splice(this.tours.indexOf(item),1);
+            else this.routes.splice(id,1);
         },
         getStatusColor(status){
             if(status == "Fail") return "red"
@@ -189,12 +318,15 @@ export default {
             this.dialogTicket = true;
         },
         addTicket(){
-            let ticket = this.tickets.filter(item => item.cities.includes(this.fromTicket && this.toTicket))[0];
-            if(ticket.type == Types.ROUTE) this.routes.push(ticket);
-            else this.tours.push(ticket);
+            let ticket = (this.foundTickets.length == 1) ? this.foundTickets[0] : this.selectedTicket[0]
+            if(ticket.type == Types.ROUTE) this.routes.push({...ticket, status:"Fail"});
+            else this.tours.push({...ticket, status:"Fail"});
+            this.fromTicket = null;
         },
         closeAddTicket(){
             this.dialogTicket = false;
+            this.toTicket = null;
+            this.fromTicket = null;
         },
         toggleToDone(item){
             let id = this.routes.indexOf(item);
@@ -218,8 +350,7 @@ export default {
         }
     },
     mounted(){
-        this.tickets = Tickets.world
-        console.log(this.tickets)
+        this.tickets = Tickets.World
     }
   }
 </script>
