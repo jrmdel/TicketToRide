90a70b16bd0540623ac803e11b18ea5cb6e493c8
jrmdel <jeremie.deletraz@gmail.com>
2022-08-09 01:01:40 +0200

    functional mock behavior of component yourcities

--- src/components/currentgame/YourCities.vue
+++ src/components/currentgame/YourCities.vue
@@ -6,30 +6,89 @@
             <v-icon large>mdi-city-variant</v-icon>
         </v-toolbar>
         <v-card-subtitle>
-            <Indicators :leftText="$t('current.cities.indicators.left')" :leftIndicator="2"
+            <Indicators :leftText="$t('current.cities.indicators.left')" :leftIndicator="cities.length"
                 :centerCondition="false"
-                :rightText="$t('current.cities.indicators.right')" :rightIndicator="3" />
+                :rightText="$t('current.cities.indicators.right')" :rightIndicator="computedCityScore" />
         </v-card-subtitle>
         <v-card-text>
             <v-container fluid>
                 <TwoButtons
-                    :leftDisabledCondition="cities.length>2" leftColor="primary" leftIcon="mdi-domain-plus" :leftText="$t('main.btn.add-harbor')" @clickLeft="openAddCities"
+                    :leftDisabledCondition="cities.length>2" leftColor="primary" leftIcon="mdi-domain-plus" :leftText="$t('main.btn.add-city-marker')" @clickLeft="openAddCity"
                     rightColor="accent" rightIcon="mdi-restore" :rightText="$t('main.btn.reset')" @clickRight="resetCities"/>
+                <v-col cols="12">
+                    <v-row>
+                        <span class="text-h6 tertiary--text">{{$t('current.cities.subtitle')}}</span>
+                    </v-row>
+                    <v-row>
+                        <v-col v-for="i in 3" :key="i" cols="12" md="4">
+                            <v-card outlined :disabled="cities.length<i">
+                                <v-card-title>
+                                    <span>{{$t('current.cities.card.title', {id:i})}}</span>
+                                    <v-spacer></v-spacer>
+                                    <v-icon v-if="!(cities.length<i)" large color="red" @click="deleteCity(i)">mdi-close</v-icon>
+                                </v-card-title>
+                                <v-card-text class="my-n3">
+                                    <v-container fluid>
+                                        <v-row class="text-body-1" align="center">
+                                            <v-col cols="12">
+                                                <v-icon :color="(cities.length<i) ? undefined : 'primary'" class="mx-4">mdi-city-variant</v-icon>
+                                                <span v-if="cities.length>i-1">{{cities[i-1]}}</span>
+                                                <span v-else>{{$t('current.cities.card.no-city')}}</span>
+                                            </v-col>
+                                        </v-row>
+                                        <v-row class="text-body-1" align="center">
+                                            <v-col cols="12">
+                                                <v-icon :color="(cities.length<i) ? undefined : 'accent'" class="mx-4">mdi-medal-outline</v-icon>
+                                                <span v-if="cities.length>i-1">{{getCityScore(cities[i-1])}}</span>
+                                                <span v-else>0</span>
+                                            </v-col>
+                                        </v-row>
+                                    </v-container>
+                                </v-card-text>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-col>
             </v-container>
         </v-card-text>
     </v-card>
 </template>
 
 <script>
-
+import TwoButtons from "./TwoButtons.vue";
 import Indicators from "./Indicators";
 
 export default {
     components:{
-        Indicators
+        Indicators,
+        TwoButtons
     },
+    data: () => ({
+        cities: [],
+    }),
     props: {},
+    computed: {
+        computedCityScore: {
+            get() {
+                return 0;
+            },
+        }
+    },
     methods: {
+        resetCities() {
+            this.cities = [];
+        },
+        openAddCity() {
+            this.cities.push(parseInt(100*Math.random()));
+        },
+        deleteCity(index) {
+            this.cities.splice(index-1, 1);
+        },
+        getCityScore(city) {
+            console.log(city);
+            return 1;
+        },
+        // Useless so far
         updateValue(update) {
             let evnt = { units: this.numberOfUnits, update: update }
             this.$emit("update-value", evnt)
